ada277d0c472c010e24219fbff291fdb8dde7b3a
Anderson Sa <asilvadesa@gmail.com>
2026-02-26 06:05:28 -0300

    Add stragies for find elements

--- tests/tasks.spec.ts
+++ tests/tasks.spec.ts
@@ -1,8 +1,20 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Tasks feature', () => {
-    test('create a new task', async ({ page }) => {
+    test('find element by id', async ({ page }) => {
         await page.goto('http://localhost:8080/');
         await page.fill('#newTask', 'Ler um livro de TypeScript');
     });
+
+  test('find element by select css', async ({ page }) => {
+        await page.goto('http://localhost:8080/');;
+        await page.fill('input[placeholder="Add a new Task"]', 'Ler um livro de TypeScript');
+    });
+
+      test('find element by regular expression', async ({ page }) => {
+        await page.goto('http://localhost:8080/');
+        await page.fill('input[class*=InputNewTask]', 'Ler um livro de TypeScript');
+    });
+
+
 });
